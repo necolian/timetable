8eac1dc7132a5f42f561acaa62cc2061cb6537df
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-07-03 11:14:51 +0000

    index.jsをrender用に調整

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 //諸々読み込み
 const expess = require("express");
 const app = expess();
-const timetabledata = require("./public/data/kitasukematsu.json");
 const serverless = require("./public/js/serverless.js");
 
 //getの処理
 app.get("/get", (req, res) => {
   var data = "";
+  var timetable = "";
 
   //クエリで日と時間帯を取得
   const query = req.query;
 
   if (query) {
-  if ("date" in query) {
-    //日付が指定されている場合
-    if ("time" in query) {
-      //日付と時間帯が指定されている場合
-      data = timetabledata[query.date][query.time];
-    } else {
-      //日付が指定されているが時間帯が指定されていない場合
-      data = timetabledata[query.date];
+    if ("sta" in query) {
+      try {
+        timetable = require("./public/data/" + query.sta + ".json");
+        // myModule をここで使用
+      } catch (error) {
+        res.status("400").send({"code":400,"message":"その駅のデータは存在しません"});
+      }
+    }else{
+      res.status("400").send({"code":400,"message":"駅が指定されていません"});
     }
-  } else {
-    data = timetabledata;
-  }}else{
-    data = timetabledata;
+      
+    if ("date" in query) {
+      //日付が指定されている場合
+        if ("time" in query) {
+          //日付と時間帯が指定されている場合
+          data = timetable[query.date][query.time];
+        } else {
+          //日付が指定されているが時間帯が指定されていない場合
+          data = timetable[query.date];
+        }
+     } else {
+      data = timetable;
+     }
+  }else{
+    res.status("400").send({"code":400,"message":"駅が指定されていません"});
   }
 
   res.status("200").send(data);
   console.log("sended data.");
 });
 
-//サーバーレス
-exports.handler = async (event, context) => {
-  return await serverless.handler(event, context);
-};
 
 //サーバーの処理
 app.listen(3000, () => {
--- public/js/serverless.js
+++ public/js/serverless.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+  cosole.log("OK");
+  res.send("OK");
+});
+
 // サーバーレス関数の処理
 exports.handler = async (event, context) => {
   try {
